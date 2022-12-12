7db3d9a89896e884fca6436bbd0593e788febe51
fcrncalo1 <fcrncalo1@etf.unsa.ba>
2022-12-12 12:05:07 +0100

    Small change to OrdersDao

--- src/main/java/ba/unsa/etf/rpr/dao/OrdersDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/OrdersDao.java
@@ -1,9 +1,11 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Orders;
+
 import java.util.Date;
 import java.util.List;
 
-public interface OrdersDao extends Dao {
+public interface OrdersDao extends Dao<Orders> {
 
-    List getByDateRange(Date start, Date end);
+    List<Orders> getByDateRange(Date start, Date end);
 }
