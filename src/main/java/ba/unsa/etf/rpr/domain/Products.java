f1ceebbf73e8daccfb57007f85d5d23794858a39
fcrncalo1 <fcrncalo1@etf.unsa.ba>
2022-12-12 02:23:02 +0100

    Added Products class for the domain model

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Products.java
@@ -0,0 +1,66 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+public class Products {
+
+    private int id;
+    private String name;
+    private Double price;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Products{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Products products = (Products) o;
+        return id == products.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, quantity);
+    }
+}
