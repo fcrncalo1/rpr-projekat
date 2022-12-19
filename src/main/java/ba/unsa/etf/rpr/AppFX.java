8e63a98871b50d0f4d6726786ba13af1b4ae86e2
fcrncalo1 <fcrncalo1@etf.unsa.ba>
2022-12-19 01:57:19 +0100

    Added test login form

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,4 +1,25 @@
 package ba.unsa.etf.rpr;
 
-public class AppFX {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class AppFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Parent root = fxmlLoader.load();
+        stage.setTitle("Login");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -0,0 +1,50 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class LoginController {
+    public TextField fieldUsername;
+
+    @FXML
+    public void initialize() {
+        fieldUsername.getStyleClass().add("poljeNijeIspravno");
+        fieldUsername.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (fieldUsername.getText().trim().isEmpty()) {
+                    fieldUsername.getStyleClass().removeAll("poljeJeIspravno");
+                    fieldUsername.getStyleClass().add("poljeNijeIspravno");
+                } else {
+                    fieldUsername.getStyleClass().removeAll("poljeNijeIspravno");
+                    fieldUsername.getStyleClass().add("poljeJeIspravno");
+                }
+            }
+        });
+    }
+
+    public void loginBttnClick(ActionEvent actionEvent) throws IOException {
+        if (fieldUsername.getText().isEmpty()) {
+            return;
+        }
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/window.fxml"));
+        Parent root = loader.load();
+        WindowController windowController = loader.getController();
+        windowController.labela.setText(windowController.labela.getText() + fieldUsername.getText());
+        stage.setTitle("Welcome");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.show();
+    }
+}
