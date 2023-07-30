f4c989aefade100e46012e00221174884d002de6
fcrncalo1 <fcrncalo1@etf.unsa.ba>
2023-07-30 23:56:01 +0200

    added user management functionalities

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddUpdateUserController.java
@@ -0,0 +1,128 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.business.UsersManager;
+import ba.unsa.etf.rpr.domain.Users;
+import ba.unsa.etf.rpr.exceptions.UsersException;
+import ba.unsa.etf.rpr.model.UsersModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import org.apache.commons.validator.routines.EmailValidator;
+
+public class AddUpdateUserController {
+    public TextField firstNameField;
+    public TextField lastNameField;
+    public TextField emailField;
+    public TextField cityField;
+    public TextField addressField;
+    public TextField phoneField;
+    public TextField usernameField;
+    public TextField passwordField;
+    private final UsersManager usersManager = new UsersManager();
+    public GridPane addupdateGridPane;
+    private UsersModel usersModel = new UsersModel();
+    private Integer uId;
+
+    public Integer getUId() {
+        return uId;
+    }
+
+    public void setUId(Integer uId) {
+        this.uId = uId;
+    }
+
+    public AddUpdateUserController() {
+        uId = null;
+    }
+    public AddUpdateUserController(Integer uId) {
+        this.uId = uId;
+    }
+
+    @FXML
+    public void initialize() {
+        firstNameField.textProperty().bindBidirectional(usersModel.firstNameFieldProperty());
+        lastNameField.textProperty().bindBidirectional(usersModel.lastNameFieldProperty());
+        emailField.textProperty().bindBidirectional(usersModel.emailFieldProperty());
+        cityField.textProperty().bindBidirectional(usersModel.cityFieldProperty());
+        addressField.textProperty().bindBidirectional(usersModel.addressFieldProperty());
+        phoneField.textProperty().bindBidirectional(usersModel.numberFieldProperty());
+        usernameField.textProperty().bindBidirectional(usersModel.usernameFieldProperty());
+        passwordField.textProperty().bindBidirectional(usersModel.passwordFieldProperty());
+
+        if (uId != null) {
+            try {
+                usersModel.fromUser(usersManager.getById(uId));
+            } catch (UsersException e) {
+                new Alert(Alert.AlertType.ERROR,e.getMessage(), ButtonType.OK).show();
+            }
+        }
+    }
+
+    public void okClick(ActionEvent actionEvent) {
+        Users u = usersModel.toUser();
+        try {
+            if (uId != null) {
+                u.setId(uId);
+                usersManager.update(u);
+                addupdateGridPane.getScene().getWindow().hide();
+            } else {
+                Boolean flag = true;
+                System.out.println(firstNameField.textProperty().getValue());
+                if (firstNameField.textProperty().getValue().trim().length() == 0) {
+                    firstNameField.getStyleClass().removeAll("poljeIspravno");
+                    firstNameField.getStyleClass().add("poljeNijeIspravno");
+                    flag = false;
+                }
+                if (lastNameField.textProperty().getValue().trim().length() == 0) {
+                    lastNameField.getStyleClass().removeAll("poljeIspravno");
+                    lastNameField.getStyleClass().add("poljeNijeIspravno");
+                    flag = false;
+                }
+                if(!EmailValidator.getInstance().isValid(emailField.textProperty().getValue())) {
+                    emailField.getStyleClass().removeAll("poljeIspravno");
+                    emailField.getStyleClass().add("poljeNijeIspravno");
+                    flag = false;
+                }
+                if (cityField.textProperty().getValue().trim().length() == 0) {
+                    cityField.getStyleClass().removeAll("poljeIspravno");
+                    cityField.getStyleClass().add("poljeNijeIspravno");
+                    flag = false;
+                }
+                if (addressField.textProperty().getValue().trim().length() == 0) {
+                    addressField.getStyleClass().removeAll("poljeIspravno");
+                    addressField.getStyleClass().add("poljeNijeIspravno");
+                    flag = false;
+                }
+                if (phoneField.textProperty().getValue().trim().length() == 0) {
+                    phoneField.getStyleClass().removeAll("poljeIspravno");
+                    phoneField.getStyleClass().add("poljeNijeIspravno");
+                    flag = false;
+                }
+                if (usernameField.textProperty().getValue().trim().length() == 0) {
+                    usernameField.getStyleClass().removeAll("poljeIspravno");
+                    usernameField.getStyleClass().add("poljeNijeIspravno");
+                    flag = false;
+                }
+                if (passwordField.textProperty().getValue().trim().length() == 0) {
+                    passwordField.getStyleClass().removeAll("poljeIspravno");
+                    passwordField.getStyleClass().add("poljeNijeIspravno");
+                    flag = false;
+                }
+                if (flag){
+                    usersManager.add(u);
+                    addupdateGridPane.getScene().getWindow().hide();
+                }
+            }
+
+        } catch (UsersException e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+
+    public void cancelClick(ActionEvent actionEvent) {
+        addupdateGridPane.getScene().getWindow().hide();
+    }
+}
--- src/main/java/ba/unsa/etf/rpr/controllers/GuestMgmtController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/GuestMgmtController.java
@@ -1,15 +1,25 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.UsersManager;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Users;
+import ba.unsa.etf.rpr.exceptions.UsersException;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import java.sql.SQLException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class GuestMgmtController {
     public TableView usersTable;
@@ -23,6 +33,7 @@ public class GuestMgmtController {
     public TableColumn<Users,String> usernameColumn;
     public TableColumn<Users,String> passwordColumn;
 
+    private final UsersManager usersManager = new UsersManager();
     @FXML
     public void initialize() {
         idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
@@ -34,18 +45,59 @@ public class GuestMgmtController {
         numberColumn.setCellValueFactory(new PropertyValueFactory<>("mobileNumber"));
         usernameColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
         passwordColumn.setCellValueFactory(new PropertyValueFactory<>("password"));
+        fillGuests();
+    }
+    public void addButtonClick(ActionEvent actionEvent) {
+        addupdateScene(null);
+    }
+
+    public void updateButtonClick(ActionEvent actionEvent) {
+        Users selectedUser = (Users) usersTable.getSelectionModel().getSelectedItem();
+        if(selectedUser == null) return;
+        Integer uId = selectedUser.getId();
+        addupdateScene(uId);
+    }
+
+    public void deleteButtonClick(ActionEvent actionEvent) {
+        Users selectedUser = (Users) usersTable.getSelectionModel().getSelectedItem();
+        if (selectedUser == null) return;
+        else {
+            Integer uId = selectedUser.getId();
+            try {
+                usersManager.delete(uId);
+                fillGuests();
+                usersTable.refresh();
+            } catch (UsersException e) {
+                new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK).show();
+            }
+        }
+    }
+
+    public void fillGuests() {
         try {
             usersTable.setItems(FXCollections.observableList(DaoFactory.usersDao().getAll()));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    public void addButtonClick(ActionEvent actionEvent) {
-    }
 
-    public void updateButtonClick(ActionEvent actionEvent) {
-    }
-
-    public void deleteButtonClick(ActionEvent actionEvent) {
+    private void addupdateScene(Integer uId){
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/addupdateuser.fxml"));
+            loader.setController(new AddUpdateUserController(uId));
+            Parent root = loader.load();
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.setOnHiding(event -> {fillGuests(); usersTable.refresh();});
+            if(uId != null) {
+                stage.setTitle("Edit a user");
+            }
+            else stage.setTitle("Add a user");
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
