111c80a4eadad879a53194ca6a1b3da226bec7fd
fcrncalo1 <fcrncalo1@etf.unsa.ba>
2023-07-31 13:28:39 +0200

    added product management functionalities

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddUpdateProductController.java
@@ -0,0 +1,87 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.business.ProductsManager;
+import ba.unsa.etf.rpr.domain.Products;
+import ba.unsa.etf.rpr.exceptions.ProductsException;
+import ba.unsa.etf.rpr.model.ProductsModel;
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.util.converter.NumberStringConverter;
+
+public class AddUpdateProductController {
+    public TextField nameField;
+    public TextField priceField;
+    public TextField quantityField;
+    public GridPane addupdateGridPane;
+    private Integer pId;
+    public AddUpdateProductController(Integer pId) {
+        this.pId = pId;
+    }
+
+    public Integer getpId() {
+        return pId;
+    }
+
+    public void setpId(Integer pId) {
+        this.pId = pId;
+    }
+    private final ProductsManager productsManager = new ProductsManager();
+    private ProductsModel productsModel = new ProductsModel();
+
+    @FXML
+    public void initialize() {
+        nameField.textProperty().bindBidirectional(productsModel.nameFieldProperty());
+        Bindings.bindBidirectional(priceField.textProperty(),productsModel.priceFieldProperty(),new NumberStringConverter());
+        quantityField.textProperty().bindBidirectional(productsModel.quantityFieldProperty(),new NumberStringConverter());
+        if (pId != null) {
+            try {
+                productsModel.fromProduct(productsManager.getById(pId));
+            } catch (ProductsException e) {
+                new Alert(Alert.AlertType.ERROR,e.getMessage(), ButtonType.OK).show();
+            }
+        }
+    }
+    public void okClick(ActionEvent actionEvent) {
+        Products p = productsModel.toProduct();
+        try {
+            if (pId != null) {
+                p.setId(pId);
+                productsManager.update(p);
+                addupdateGridPane.getScene().getWindow().hide();
+            } else {
+                Boolean flag = true;
+                if (nameField.textProperty().getValue().trim().length() == 0) {
+                    nameField.getStyleClass().removeAll("poljeIspravno");
+                    nameField.getStyleClass().add("poljeNijeIspravno");
+                    flag = false;
+                }
+                if (priceField.textProperty().getValue().trim().length() == 0) {
+                    priceField.getStyleClass().removeAll("poljeIspravno");
+                    priceField.getStyleClass().add("poljeNijeIspravno");
+                    flag = false;
+                }
+                if (quantityField.textProperty().getValue().trim().length() == 0) {
+                    quantityField.getStyleClass().removeAll("poljeIspravno");
+                    quantityField.getStyleClass().add("poljeNijeIspravno");
+                    flag = false;
+                }
+                if (flag){
+                    productsManager.add(p);
+                    addupdateGridPane.getScene().getWindow().hide();
+                }
+            }
+
+        } catch (ProductsException e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+
+    public void cancelClick(ActionEvent actionEvent) {
+        addupdateGridPane.getScene().getWindow().hide();
+    }
+}
--- src/main/java/ba/unsa/etf/rpr/controllers/ProductMgmtController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProductMgmtController.java
@@ -1,14 +1,25 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.ProductsManager;
 import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Products;
+import ba.unsa.etf.rpr.exceptions.ProductsException;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import java.sql.SQLException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class ProductMgmtController {
     public TableView productsTable;
@@ -16,26 +27,67 @@ public class ProductMgmtController {
     public TableColumn nameColumn;
     public TableColumn priceColumn;
     public TableColumn quantityColumn;
-
+    private final ProductsManager productsManager = new ProductsManager();
     @FXML
     public void initialize() {
         idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
         quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        fillProducts();
+    }
+
+    public void addButtonClick(ActionEvent actionEvent) {
+        addupdateScene(null);
+    }
+
+    public void updateButtonClick(ActionEvent actionEvent) {
+        Products selectedProduct = (Products) productsTable.getSelectionModel().getSelectedItem();
+        if (selectedProduct == null) return;
+        Integer pId = selectedProduct.getId();
+        addupdateScene(pId);
+    }
+
+    public void deleteButtonClick(ActionEvent actionEvent) {
+        Products selectedProduct = (Products) productsTable.getSelectionModel().getSelectedItem();
+        if (selectedProduct == null) return;
+        else {
+            Integer pId = selectedProduct.getId();
+            try {
+                productsManager.delete(pId);
+                fillProducts();
+                productsTable.refresh();
+            } catch (ProductsException e) {
+                new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK).show();
+            }
+        }
+    }
+
+    private void fillProducts() {
         try {
             productsTable.setItems(FXCollections.observableList(DaoFactory.productsDao().getAll()));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-
-    public void addButtonClick(ActionEvent actionEvent) {
-    }
-
-    public void updateButtonClick(ActionEvent actionEvent) {
-    }
-
-    public void deleteButtonClick(ActionEvent actionEvent) {
+    private void addupdateScene(Integer pId) {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/addupdateproduct.fxml"));
+            loader.setController(new AddUpdateProductController(pId));
+            Parent root = loader.load();
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.setOnHiding(event -> {
+                fillProducts();
+                productsTable.refresh();
+            });
+            if (pId != null) {
+                stage.setTitle("Edit a product");
+            } else stage.setTitle("Add a product");
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
